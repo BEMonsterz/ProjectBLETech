f74740961c1094f95124b422cfec7d5c6fab2d06
Bryan Ayllon <bryanayllonroa@gmail.com>
2014-12-07 16:28:37 -0600

    projext day 3

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,1 @@
 //= require_tree .
-
-var activeEl = 2;
-$(function() {
-    var items = $('.btn-nav');
-    $( items[activeEl] ).addClass('active');
-    $( ".btn-nav" ).click(function() {
-        $( items[activeEl] ).removeClass('active');
-        $( this ).addClass('active');
-        activeEl = $( ".btn-nav" ).index( this );
-    });
-});
